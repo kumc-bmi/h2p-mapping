bf95e0493c5101c05921ea8a8271a6f0958462e7
Lav Patel <lpatel@kumc.edu>
2017-10-31 11:34:21 -0500

    4850: spool was adding space which was creting issues for tomcat to import the csv

--- Oracle/shrine_AdapterMappings.sh
+++ Oracle/shrine_AdapterMappings.sh
@@ -12,33 +12,9 @@ sqlplus -S $USERNAME/$PASSWORD@$SID @shrine_AdapterMappings.sql
 # exporting shrine ont mapping as CSV and then comnine with manual mapping
 ##########################################################################
 
-sqlplus -S $USERNAME/$PASSWORD@$SID << EOF | grep -E "^ORA-|^ERROR"
-select systimestamp from dual;
-
-set termout off
-set arraysize 1000
--- set rowprefetch 2
-set pages 0
-SET echo off
-SET feedback off
-SET pagesize 0
-SET linesize 32000
-SET sqlprompt ''
-SET trimspool on
-set term off
-set feed off
-SPOOL AdapterMappings.csv
-select '"'|| SCIHLS_PATH || '","'|| HERON_PATH || '"' from  SHRINE_ONT.TEMP_SHRINE_MAPPING;
-SPOOL off
-
-set termout on
-select systimestamp from dual;
-
-exit
-EOF
-
-tail -n+2 AdapterMappings.csv  > temp.csv
-head -n-1 temp.csv  > AdapterMappings.csv
-rm temp.csv
+sqlplus -S $USERNAME/$PASSWORD@$SID @shrine_export_AdapterMapping.sql
+# removing empty lines from csv
+sed '/^\s*$/d' AdapterMappings.csv  > temp.csv
+mv temp.csv AdapterMappings.csv
 
 cat shrine_manual_AdapterMappings.csv >> AdapterMappings.csv
