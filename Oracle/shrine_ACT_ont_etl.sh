46cd6898bd816dd4689b0d4da30fc474bc678c19
lpatel <lpatel@yellowfin.kumc.edu>
2019-08-26 11:54:38 -0500

    oops (5894)

--- Oracle/shrine_ACT_ont_etl.sh
+++ Oracle/shrine_ACT_ont_etl.sh
@@ -46,4 +46,4 @@ fi
 
 sqlplus $USERNAME/$PASSWORD@$SID @shrine_ACT_onto_index.sql
 
-#sqlplus $USERNAME/$PASSWORD@$SID @shrine_ACT_metadata_and_concept.sql $upload_id
+sqlplus $USERNAME/$PASSWORD@$SID @shrine_ACT_metadata_and_concept.sql $upload_id
