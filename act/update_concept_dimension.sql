b6f84ddf074f78dfd8b254212db1736b5c144c1f
Lav Patel <contact.lavpatel@gmail.com>
2020-11-17 20:07:11 -0600

    only currently tested tables are being added to concept_dim

--- act/update_concept_dimension.sql
+++ act/update_concept_dimension.sql
@@ -32,7 +32,8 @@ insert  /*+  APPEND */ into "&&heron_data_schema".concept_dimension(
   sourcesystem_cd,
   upload_id
   )
-select /*+  PARALLEL */ distinct 
+select 
+  -- distinct --asuming if not distinct it will fail by pk
   ib.c_basecode,
   -- Previously, ib.c_fullname was selected instead of ib.c_dimcode.  
   -- This was incorrect, because concept searches look for c_dimcode, 
@@ -52,18 +53,18 @@ from
 (
 -- sql generator: select  'select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".' ||c_table_name ||' union all'  sql from shrine_ont_act.table_access order by sql;
 select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_COVID union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_CPT_PX_2018AA union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_HCPCS_PX_2018AA union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_CPT_PX_2018AA union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_HCPCS_PX_2018AA union all
 select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_ICD10CM_DX_2018AA union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_ICD10PCS_PX_2018AA union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_ICD10PCS_PX_2018AA union all
 select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_ICD9CM_DX_2018AA union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_ICD9CM_PX_2018AA union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_LOINC_LAB_2018AA union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_MED_ALPHA_V2_121318 union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_MED_VA_V2_092818 union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".NCATS_DEMOGRAPHICS union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_ICD9CM_PX_2018AA union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_LOINC_LAB_2018AA union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_MED_ALPHA_V2_121318 union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".ACT_MED_VA_V2_092818 union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".NCATS_DEMOGRAPHICS union all
 select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".NCATS_ICD10_ICD9_DX_V1 union all
-select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".NCATS_LABS union all
+--select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".NCATS_LABS union all
 select C_BASECODE, C_FULLNAME, C_NAME, UPDATE_DATE, DOWNLOAD_DATE, sourcesystem_cd, C_DIMCODE from "&&metadata_schema".NCATS_VISIT_DETAILS
 ) ib
 where ib.c_basecode is not null
