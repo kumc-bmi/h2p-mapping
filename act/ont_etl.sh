37dd55ed0f1e72700e2889605367c2d5a5e3f3c7
Lav Patel <contact.lavpatel@gmail.com>
2020-11-19 14:03:36 -0600

    seprated out demographic mapping

--- act/ont_etl.sh
+++ act/ont_etl.sh
@@ -18,7 +18,8 @@ set -euxo pipefail
 sqlplus $USERNAME/$PASSWORD@$SID @create_metadata_tables_from_shrine_ont.sql $metadata_schema $shrine_ont_schema
 sqlplus $USERNAME/$PASSWORD@$SID @update_table_access.sql $metadata_schema
 sqlplus $USERNAME/$PASSWORD@$SID @diagnosis_mapping.sql $metadata_schema
-sqlplus $USERNAME/$PASSWORD@$SID @metadata_and_concept.sql $upload_id $heron_data_schema $shrine_ont_schema
+sqlplus $USERNAME/$PASSWORD@$SID @demographics_mapping.sql $metadata_schema
 sqlplus $USERNAME/$PASSWORD@$SID @procedure_ont_map.sql
 
+sqlplus $USERNAME/$PASSWORD@$SID @metadata_and_concept.sql $upload_id $heron_data_schema $shrine_ont_schema
 sqlplus $USERNAME/$PASSWORD@$SID @update_concept_dimension.sql $upload_id $heron_data_schema $metadata_schema
